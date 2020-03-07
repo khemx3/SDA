f2f9b4e754615958ab3d22a797afe3f7e34e47a1
Phakapol Chuliang <phakapol_chuliang@yahoo.com>
2020-03-07 20:56:28 +0700

    create CareTaker class

--- src/project1/CareTaker.java
+++ src/project1/CareTaker.java
@@ -1,4 +1,22 @@
 package project1;
 
+import java.io.*;
+
 public class CareTaker {
+    private Object object;
+    private String temporaryFileName = "temporaryInventory.ser";
+    private String fileName = "Inventory.ser";
+    private File tempFile = new File(temporaryFileName);
+    private File file = new File(fileName);
+    byte[] buf = new byte[1024];
+    int bytesRead;
+
+    public void serializeMemento(Memento state) {
+        // object to stream
+    }
+
+    public Memento deserializeMemento() {
+        // stream to object
+        return null;
+    }
 }
